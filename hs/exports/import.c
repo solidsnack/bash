164f570bbb260146328d65a925e96aeb87a33f97
Jason Dusek <jason.dusek@gmail.com>
2011-03-09 21:01:22 +0000

    Works now in conjunction with Makefile.

--- hs/exports/import.c
+++ hs/exports/import.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
-#include "./exports_stub.h"
-#include "./exportsinit.c"
+#include "HsFFI.h"
 
-int main()
+#ifdef __GLASGOW_HASKELL__
+#include "exports_stub.h"
+#endif
+
+#ifdef __GLASGOW_HASKELL__
+extern void __stginit_LanguageBashExports(void);
+#endif
+
+int main(int argc, char *argv[])
 {
-  LanguageBashExports_init();
+  int i;
+
+  hs_init(&argc, &argv);
+#ifdef __GLASGOW_HASKELL__
+  hs_add_root(__stginit_LanguageBashExports);
+#endif
+
   HsWord16 num = hs42();
-  LanguageBashExports_end();
   printf("Hello %d\n", num);
+
+  hs_exit();
   return 0;
 }
 
