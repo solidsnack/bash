e199d48a9d02d2aa6367adc479625d25ac3615e4
Jason Dusek <jason.dusek@gmail.com>
2011-03-11 18:37:02 +0000

    Cleaning this out; it will be handled elsewhere.

--- hs/well-typed/fooinit.c
+++
@@ -1,9 +0,0 @@
-#include "HsFFI.h"
-
-extern void __stginit_Foo(void);
-static void Foo_init (void) __attribute__ ((constructor));
-void Foo_init (void) {
-  hs_init(0, 0);
-  hs_add_root(__stginit_Foo);
-}
-
